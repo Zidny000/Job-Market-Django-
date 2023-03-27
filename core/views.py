59ef19f1611d92f831fe1da89e0b82c6803bdabb
zidny000 <t.m.zidny@gmail.com>
2023-03-27 23:07:08 +0600

    Django Project

---
+++ core/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request,'core/index.html')
+
+def login(request):
+    return render(request,'core/login.html')
+
+def signup(request):
+    return render(request,'core/signup.html')
