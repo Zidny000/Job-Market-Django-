66e0dec745870dbda026752480d99594fde7a515
zidny000 <t.m.zidny@gmail.com>
2023-04-01 23:36:07 +0600

    Product Database Created and Implemented

--- core/views.py
+++ core/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+
+from .models import Category,Post
+
 
 # Create your views here.
 
 def index(request):
-    return render(request,'core/index.html')
+    categories = Category.objects.all()
+    posts = Post.objects.all()
+    # development_posts = Post.objects.filter(category=categories[1].pk)[0:3]
+    # bank_posts = Post.objects.filter(category=categories[0].pk)[0:3]
+    return render(request,'core/index.html',{
+        'posts' : posts,
+        # 'development_posts': development_posts,
+        # 'bank_posts' : bank_posts,
+        'categories' : categories
+    })
 
 def login(request):
     return render(request,'core/login.html')
@@ -17,5 +29,10 @@ def jobpost(request):
 def profile(request):
     return render(request,'core/profile.html')
 
-def jobdetails(request):
-    return render(request,'core/jobdetails.html')
+def jobdetails(request,pk):
+    job = get_object_or_404(Post,pk=pk)
+    related_jobs = Post.objects.filter(category=job.category).exclude(pk=pk)[0:3]
+    return render(request,'core/jobdetails.html',{
+        'job':job,
+        'related_items':related_items
+    })
--- jobMarket/urls.py
+++ jobMarket/urls.py
@@ -13,7 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -25,6 +26,6 @@ urlpatterns = [
     path('signup/',signup,name='signup'),
     path('jobs/',jobpost,name='jobpost'),
     path('profile/',profile,name='profile'),
-    path('job/',jobdetails,name='jobdetails'),
+    path('job/<int:pk>/',jobdetails,name='jobdetails'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
